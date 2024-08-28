71cf31bf6d79a37b9ab9f7b38dc07f3e32a33e5f
dmalidevs <dmalidevs@gmail.com>
2024-08-28 20:00:14 +0600

    Resolvers Created

---
+++ app/GraphQL/Mutations/CreatePostResolver.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Mutations;
+use App\Models\Post;
+
+final readonly class CreatePostResolver {
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args) {
+        return Post::create([
+            'user_id' => $args['user_id'],
+            'title'   => $args['title'],
+            'body'    => $args['body']
+        ]);
+    }
+}
---
+++ app/GraphQL/Queries/PostResolver.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+use App\Models\Post;
+
+final readonly class PostResolver {
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args) {
+        return Post::where('id', $args['id'])->first();
+    }
+}
